94dd3d7f64075f53ad6ca80fb8a113774244b2f1
Andrew <mynock101@gmail.com>
2015-01-10 15:03:35 -0600

    Add SendGrid integration and change the email header to be smaller.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'andrewlowell',
+  :password => 'Q147896325WE',
+  :domain => 'wilmettefamilyhistoryfair.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
