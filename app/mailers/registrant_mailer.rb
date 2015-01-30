2e56d09d704d62e1c80fa71ffd8bb7fd61657ad9
Andrew <mynock101@gmail.com>
2015-01-29 21:39:23 -0600

    Hopefully fix stuff.

--- app/mailers/registrant_mailer.rb
+++ app/mailers/registrant_mailer.rb
@@ -2,10 +2,10 @@ class RegistrantMailer < ApplicationMailer
   default from: 'registration@chicagofamilyhistoryfair.com'
   def registration_email(registrant)
     @registrant = registrant
-    mail(to: @registrant.email_address, subject: 'Thanks for registering for The Wilmette Family History Fair!')
+    mail(to: @registrant.email_address, subject: 'Thanks for registering for The Chicago Family History Fair!')
   end
   def registration_notification(registrant)
     @registrant = registrant
-    mail(to: "dean@genlighten.com", subject: "CFHF-2015 New Registration")
+    mail(to: "mynock101@gmail.com", subject: "CFHF-2015 New Registration")
   end
 end
