a9dc1c95489c2416ef9790b46559a030d97f5d92
Andrew <mynock101@gmail.com>
2015-03-06 16:51:35 -0600

    Add new classes, new PDF, fix schedule talking about lunch.

--- config/initializers/CFHF-classes.rb
+++ config/initializers/CFHF-classes.rb
@@ -14,7 +14,8 @@
                 ["Hints and Sources in FamilyTree (Susan Anderson - Family History Center)", "Hints and Sources in FamilyTree (<a href=\"/speakers#bruce_susan\">Susan Anderson</a> - Family History Center)"],
                 ["Finding Missing People in Your Family Tree (Video - Room 1)"],
                 ["Buscando a personas perdidas en mi Arbol Familiar (Video - Room 2)"],
-                ["Family History on the Go (Video - Room 3)"],
+                ["Map My Ancestors (Video - Room 3)"],
+                #["Family History on the Go (Video - Room 3)"],
                 ["1-on-1 Research Help (Room 4)"]
               ],
               
@@ -25,23 +26,28 @@
                 ["From Tree to Temple: Preparing Family Names for the Temple (Video - Room 1)"],
                 ["Del Arbol al Templo: Preparando los nombres de nuestros familiares para el Templo (Video - Room 2)"],
                 ["Getting Started in Genetic Genealogy (Video - Room 3)"],
-                ["1-on-1 Research Help (Room 4)"],
+                ["1-on-1 Research Help (Room 4)"]
               ],
               
               [
                 ["You Use WHAT for Genealogy? Wonderful Uses for Unusual Tools (Thomas MacEntee - Chapel)"],
                 ["Online Census Tips and Secrets (Donna Roginski - Relief Society Room)", "Online Census Tips and Secrets (<a href=\"/speakers#donna_roginski\">Donna Roginski</a> - Relief Society Room)"],
                 ["RootsMagic: Sourcing, Adding Media, & Linking to FamilySearch (Julie Busse - Family History Center)"],
-                ["FamilySearch & Beyond: New Resources for Members (Video - Room 1)"],
+                ["LDS Research Ideas: Family Discovery Day --Priesthood and Auxiliaries Session (Video - Room 1)"],
+                #["FamilySearch & Beyond: New Resources for Members (Video - Room 1)"],
                 ["FamilySearch y mas alla: Nuevos recursos para miembros (Video - Room 2)"],
-                ["FamilySearch Indexing: It's a Whole New World! (Video - Room 3)"],
+                ["Choreograph Your Research Across the Internet (Video - Room 3)"],
+                #["FamilySearch Indexing: It's a Whole New World! (Video - Room 3)"],
                 ["1-on-1 Research Help (Room 4)"]
               ],
               
               [
+                ["Building a Genealogy Research Toolbox - Thomas MacEntee (Video - Chapel)"],
+                ["FamilySearch Family Tree Now and in the Future (Video - Relief Society Room)"],
                 ["Branching Out: Useful Tools to Help You Find More Names (Video - Room 1)"],
                 ["Diversifique su busqueda: Herramientas utiles para ayudarles a encontrar mas nombres (Video - Room 2)"],
-                ["Building a Genealogy Research Toolbox (Video - Room 3)"],
+                ["You've Mastered the Census and Basic Search, What Next? (Video - Room 3)"],
+                #["Building a Genealogy Research Toolbox (Video - Room 3)"],
                 ["1-on-1 Research Help (Room 4)"]
               ]
               
