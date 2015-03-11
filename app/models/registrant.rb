7c02cfdff241a0f86804b1e70374c505d17420f6
Andrew <mynock101@gmail.com>
2015-03-10 21:47:49 -0500

    Fix possible bug not allowing Bonnie Blohm to register, email maximum characters to 80 rather than 50.

--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -2,7 +2,7 @@ class Registrant < ActiveRecord::Base
   #attr_accessor :full_name, :email_address, :time_slot_1, :time_slot_2, :time_slot_3, :time_slot_4, :time_slot_5
   
   before_save { email_address.downcase! }
-  validates :full_name,  presence: true, length: { maximum: 50 }
+  validates :full_name,  presence: true, length: { maximum: 80 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email_address, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
